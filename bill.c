9d347af9952242ee07a09b5a8718a2f0ce7c918a
Eugene Kim <eugenekim@cpp.edu>
2022-10-21 14:57:42 -0700

    random number bill generator created

--- bill.c
+++ bill.c
@@ -19,3 +19,41 @@
         - total bill 
 
 */
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(int argc, char *argv[])
+{
+
+    if(argc != 3){
+        // enter here when invaild number of argument received
+        printf("Please enter the tax and tip\n");
+    }
+    else{
+        // enter here when you only received 3 arguments 
+
+        // generate the random bill
+        double menu[] = {9.95, 4.55, 13.25, 22.35};
+        const int MAX = 10; // max number of order
+        int n; // actial number of order
+        int bill = 0; // used to store bill
+        
+
+        time_t t; 
+
+        srand((unsinged) time(&t)); // Init. RNG
+
+        n = (rand() % 10) + 1; // order range 1-10
+
+        for(int i = 0; i < n; ++i){
+            int j = rand() % 4; // range 0-3
+            bill = bill + menu[j]; 
+        }
+
+        // now random bill is generated. 
+
+
+    }
+
+
+})
